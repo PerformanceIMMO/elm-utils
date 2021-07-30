2ac317e7ea6519c03f7f611d75e4f6e675f97052
Nicolas Bétheuil <wadouk@gmail.com>
2021-07-30 15:44:01 +0200

    better doc

--- src/Perfimmo/Cmd/ActionToCmd.elm
+++ src/Perfimmo/Cmd/ActionToCmd.elm
@@ -1,12 +1,14 @@
 module Perfimmo.Cmd.ActionToCmd exposing (msgToElmCmd)
 
-{-|
-Launch action in one line
+{-| Launch action in one line
 
 @docs msgToElmCmd
 -}
 
 import Task
 
+{-|  Launch action in one line
+
+-}
 msgToElmCmd : a -> Cmd a
 msgToElmCmd = Task.succeed >> Task.perform identity
