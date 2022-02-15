93f68c03d9515be9b973ab9ab5241becae476ea2
Nicolas Bétheuil <wadouk@gmail.com>
2022-02-15 12:25:18 +0100

    add: html select tools

--- src/Perfimmo/Html/Dom.elm
+++ src/Perfimmo/Html/Dom.elm
@@ -1,17 +1,23 @@
 module Perfimmo.Html.Dom exposing
     ( onClick_stopProp
     , buildOptions
-    )
+    , selectViewOptions, selectView, datalistView)
 
 {-| Handle event propagation
 
-@docs onClick_stopProp, buildOptions
+@docs onClick_stopProp
+
+@docs selectView, buildOptions
+
+@docs selectViewOptions
+
+@docs datalistView
 -}
 
-import Html.Styled.Events exposing (stopPropagationOn)
+import Html.Styled.Events exposing (onInput, stopPropagationOn)
 import Json.Decode as Decode exposing (Decoder)
-import Html.Styled exposing (Html, option, text)
-import Html.Styled.Attributes exposing (id, selected, value)
+import Html.Styled exposing (Html, datalist, div, input, option, select, text)
+import Html.Styled.Attributes exposing (id, list, selected, type_, value)
 
 {-| onClick_stopProp
 -}
@@ -25,6 +31,16 @@ buildOptions value values =
     (Maybe.map (always []) value
         |> Maybe.withDefault [option [] [text "Choisir une valeur"]]) ++ List.map (buildOption value) values
 
+{-| selectView
+-}
+selectView : (String -> msg) -> List (Html msg) -> Html msg
+selectView msg = select [ onInput msg ]
+
+{-| selectViewOptions
+-}
+selectViewOptions : (String -> msg) -> Maybe String -> List (String, String) -> Html msg
+selectViewOptions msg value values = selectView msg <| buildOptions value values
+
 buildOption : Maybe String -> (String, String) -> Html msg
 buildOption v0 (id, nom) =
     option
@@ -33,3 +49,17 @@ buildOption v0 (id, nom) =
         ]
         [ text nom ]
 
+{-| datalistView
+-}
+datalistView: (String -> msg) -> String -> (List String) -> Maybe String -> Html msg
+datalistView msg listid elements currentValue = div []
+    [ input [ onInput msg, list listid, type_ "text", value <| Maybe.withDefault "" currentValue ] []
+    , datalist [ id listid ]
+        <| buildOptionsDatalist elements
+    ]
+
+buildOptionsDatalist : List String -> List (Html msg)
+buildOptionsDatalist list =
+    let mapOption l = option [value l] []
+    in (++) [option [] []]
+        <| List.map mapOption list
