4d62a6d4dea048e8df07417b6512baf885b630af
Nicolas Bétheuil <wadouk@gmail.com>
2022-02-17 17:37:30 +0100

    fixes: handle select and datalist

--- src/Perfimmo/Html/Dom.elm
+++ src/Perfimmo/Html/Dom.elm
@@ -1,23 +1,25 @@
 module Perfimmo.Html.Dom exposing
     ( onClick_stopProp
     , buildOptions
-    , selectViewOptions, selectView, datalistView)
+    , selectViewOptions, selectView, buildFilterOptions, clearableSelect, clearableSelectOptions, datalistView)
 
 {-| Handle event propagation
 
 @docs onClick_stopProp
 
-@docs selectView, buildOptions
+@docs selectView, buildOptions, buildFilterOptions
 
 @docs selectViewOptions
 
 @docs datalistView
+
+@docs clearableSelect, clearableSelectOptions
 -}
 
-import Html.Styled.Events exposing (onInput, stopPropagationOn)
+import Html.Styled.Events exposing (onClick, onInput, stopPropagationOn)
 import Json.Decode as Decode exposing (Decoder)
-import Html.Styled exposing (Html, datalist, div, input, option, select, text)
-import Html.Styled.Attributes exposing (id, list, selected, type_, value)
+import Html.Styled exposing (Html, button, datalist, div, input, option, select, text)
+import Html.Styled.Attributes exposing (class, id, list, selected, type_, value)
 
 {-| onClick_stopProp
 -}
@@ -30,6 +32,28 @@ buildOptions : Maybe String -> List (String, String) -> List (Html msg)
 buildOptions value values =
     (Maybe.map (always []) value
         |> Maybe.withDefault [option [] [text "Choisir une valeur"]]) ++ List.map (buildOption value) values
+
+{-| buildFilterOptions
+-}
+buildFilterOptions : Maybe String -> List (String, String) -> List (Html msg)
+buildFilterOptions value values =
+    [option [] [text ""]] ++ List.map (buildOption value) values
+
+{-| clearableSelect
+-}
+clearableSelect : (String -> msg) -> List (Html msg) -> Html msg
+clearableSelect msg options = div [ class "clearableSelect" ]
+    [ selectView msg options
+    , button
+        [ class "clearSelect"
+        , onClick <| msg ""]
+        [ text "x" ]
+    ]
+
+{-| clearableSelectOptions
+-}
+clearableSelectOptions : (String -> msg) -> Maybe String -> List (String, String) -> Html msg
+clearableSelectOptions msg value values = clearableSelect msg <| buildOptions value values
 
 {-| selectView
 -}
@@ -50,10 +74,13 @@ buildOption v0 (id, nom) =
         [ text nom ]
 
 {-| datalistView
+
+Current value is handle natively by DOM so it is avoided to be provided
+
 -}
-datalistView: (String -> msg) -> String -> (List String) -> Maybe String -> Html msg
-datalistView msg listid elements currentValue = div []
-    [ input [ onInput msg, list listid, type_ "text", value <| Maybe.withDefault "" currentValue ] []
+datalistView: (String -> msg) -> String -> (List String) -> Html msg
+datalistView msg listid elements = div []
+    [ input [ onInput msg, list listid, type_ "text" ] []
     , datalist [ id listid ]
         <| buildOptionsDatalist elements
     ]
